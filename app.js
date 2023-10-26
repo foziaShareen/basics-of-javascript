98aa0886605beb0f5a83d4946988a27737b41c4a
fozia shareen <foziashareen1@gmail.com>
2023-10-26 22:50:41 +0500

    added basic1

---
+++ app.js
@@ -0,0 +1,9 @@
+//document.write("hello");
+// alert("something")
+//console.log("hi to everyone")
+// note
+// just see the power of console as compare to alert and document.write
+// suppose we have an object
+document.write({name:"fozoa"});
+alert({name:"fozia"});
+console.log({name:"fozia"})
